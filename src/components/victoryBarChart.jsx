060c38e9cfe610d2e6ed68c7b2e60c934311c041
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2020-04-19 13:19:08 +1000

    VictoryBar component adedd with default sample data

--- src/components/victoryBarChart.jsx
+++ src/components/victoryBarChart.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { VictoryBar, VictoryChart } from "victory";
+
+const data = [
+  { quarter: 1, earnings: 3000 },
+  { quarter: 2, earnings: 8500 },
+  { quarter: 3, earnings: 4250 },
+  { quarter: 4, earnings: 6000 },
+];
 
 class VictoryBarChart extends Component {
-  state = {};
   render() {
-    return <h4>Victory Bar Chart </h4>;
+    return (
+      <VictoryChart>
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
+    );
   }
 }
 
