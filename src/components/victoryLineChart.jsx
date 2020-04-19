257328563092ab73360ee47eb0eb0c8db3f68c9d
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2020-04-19 22:30:15 +1000

    victory line chart showing up with local data

--- src/components/victoryLineChart.jsx
+++ src/components/victoryLineChart.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { VictoryChart, VictoryTheme, VictoryLine } from "victory";
 
 class VictoryLineChart extends Component {
-  state = {};
+  state = {
+    data: [],
+  };
+  componentDidMount() {
+    const data = [
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 },
+    ];
+
+    this.setState({ data });
+  }
   render() {
-    return <h3>Line Chart Victory</h3>;
+    return (
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine data={this.state.data}></VictoryLine>
+      </VictoryChart>
+    );
   }
 }
 
