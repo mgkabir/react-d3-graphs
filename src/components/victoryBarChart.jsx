243747e41fbbbacdd5faa6c079112086a6ee24e1
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2020-04-19 21:29:20 +1000

    style added victory barChart

--- src/components/victoryBarChart.jsx
+++ src/components/victoryBarChart.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import { VictoryBar, VictoryChart } from "victory";
-
-const data = [
-  { quarter: 1, earnings: 3000 },
-  { quarter: 2, earnings: 8500 },
-  { quarter: 3, earnings: 4250 },
-  { quarter: 4, earnings: 6000 },
-];
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
 
 class VictoryBarChart extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    const retrievedData = [
+      { quarter: 1, earnings: 2100 },
+      { quarter: 2, earnings: 3500 },
+      { quarter: 3, earnings: 4250 },
+      { quarter: 4, earnings: 2000 },
+    ];
+
+    this.setState({ data: retrievedData });
+  }
+
   render() {
     return (
-      <VictoryChart>
-        <VictoryBar data={data} x="quarter" y="earnings" />
+      <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+        <VictoryAxis
+          // tickValues specifies both the number of ticks and where
+          // they are placed on the axis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          // tickFormat specifies how ticks should be displayed
+          tickFormat={(x) => `$${x / 1000}k`}
+        />
+        <VictoryBar data={this.state.data} x="quarter" y="earnings" />
       </VictoryChart>
     );
   }
