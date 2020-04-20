bc7ecac36d588332c4824c22c20c7c1f1475032e
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2020-04-20 11:49:22 +1000

    Axis added to victory line chart

--- src/components/victoryLineChart.jsx
+++ src/components/victoryLineChart.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
-import { VictoryChart, VictoryTheme, VictoryLine } from "victory";
+import { VictoryChart, VictoryTheme, VictoryLine, VictoryAxis } from "victory";
 import axios from "axios";
 
+const apiEndpoint =
+  "https://api.covid19api.com/dayone/country/bangladesh/status/confirmed/live";
 class VictoryLineChart extends Component {
   state = {
     lineData: [],
   };
   async componentDidMount() {
-    const { data } = await axios.get(
-      "https://api.covid19api.com/dayone/country/malawi/status/confirmed/live"
-    );
+    const { data } = await axios.get(apiEndpoint);
     const dataArray = [...data];
     const newArray = dataArray.map((d) => {
       return { x: d.Date, y: d.Cases };
     });
     this.setState({ lineData: newArray });
   }
+
   render() {
     return (
       <VictoryChart theme={VictoryTheme.material}>
+        <VictoryAxis tickValues={[]} tickFormat={(x) => x} />
+        <VictoryAxis dependentAxis tickFormat={(x) => `$${x / 1000}K`} />
         <VictoryLine data={this.state.lineData}></VictoryLine>
       </VictoryChart>
     );
