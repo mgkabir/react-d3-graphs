6b0d50b885ad846bf964db1d45fbdd7e700b795f
Mohammad Kabir <mgkabir@gmail.com>
2020-04-23 22:01:35 +1000

    Satck of Bars used to show new confirms, recovered and deaths

--- src/components/victoryBarChart.jsx
+++ src/components/victoryBarChart.jsx
@@ -6,33 +6,60 @@ import {
   VictoryTheme,
   VictoryStack,
 } from "victory";
+import axios from "axios";
+
+const apiEndPoint =
+  "https://api.covid19api.com/country/bangladesh?from=2020-04-05T00:00:00Z&to=2020-04-23T00:00:00Z";
 
 class VictoryBarChart extends Component {
   state = {
-    dataSetOne: [],
-    dataSetTwo: [],
+    newConfirmed: [],
+    newRecovered: [],
+    newDeaths: [],
   };
 
-  componentDidMount() {
-    const dataSetOne = [
-      { quarter: new Date(2000, 2, 2), earnings: 210 },
-      { quarter: new Date(2000, 3, 3), earnings: 350 },
-      { quarter: new Date(2000, 5, 4), earnings: 420 },
-      { quarter: new Date(2000, 6, 5), earnings: 200 },
-    ];
-    const dataSetTwo = [
-      { quarter: new Date(2000, 2, 2), earnings: 100 },
-      { quarter: new Date(2000, 3, 3), earnings: 500 },
-      { quarter: new Date(2000, 5, 4), earnings: 250 },
-      { quarter: new Date(2000, 6, 5), earnings: 600 },
-    ];
+  async componentDidMount() {
+    const { data } = await axios.get(apiEndPoint);
+    const newConfirmed = [];
+    const newRecovered = [];
+    const newDeaths = [];
+    let prevConf = 0;
+    let prevReco = 0;
+    let prevDeath = 0;
+    data.forEach((element) => {
+      const {
+        Confirmed: accConfirmed,
+        Deaths: accDeaths,
+        Recovered: accRecovered,
+        Date: evtDate,
+      } = element;
 
-    this.setState({ dataSetOne, dataSetTwo });
+      if (prevConf !== 0) {
+        newConfirmed.push({
+          day: new Date(evtDate),
+          confirmed: accConfirmed - prevConf,
+        });
+        newRecovered.push({
+          day: new Date(evtDate),
+          recovered: accRecovered - prevReco,
+        });
+        newDeaths.push({
+          day: new Date(evtDate),
+          deaths: accDeaths - prevDeath,
+        });
+      }
+
+      prevConf = accConfirmed;
+      prevReco = accRecovered;
+      prevDeath = accDeaths;
+    });
+
+    this.setState({ newConfirmed, newRecovered, newDeaths });
   }
 
   render() {
     return (
-      <div class="w-75">
+      <div className="w-75">
         <VictoryChart
           theme={VictoryTheme.material}
           domainPadding={20}
@@ -41,9 +68,10 @@ class VictoryBarChart extends Component {
           <VictoryAxis />
           <VictoryAxis dependentAxis />
 
-          <VictoryStack colorScale={["orange", "steelblue"]}>
-            <VictoryBar data={this.state.dataSetOne} x="quarter" y="earnings" />
-            <VictoryBar data={this.state.dataSetTwo} x="quarter" y="earnings" />
+          <VictoryStack colorScale={["orange", "steelblue", "red"]}>
+            <VictoryBar data={this.state.newConfirmed} x="day" y="confirmed" />
+            <VictoryBar data={this.state.newRecovered} x="day" y="recovered" />
+            <VictoryBar data={this.state.newDeaths} x="day" y="deaths" />
           </VictoryStack>
         </VictoryChart>
       </div>
