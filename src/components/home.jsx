938fe211d5e13376d70c0feac5c8387de9bbea90
Mohammad Kabir <mgkabir@gmail.com>
2020-04-28 13:16:47 +1000

    datajoin in d3 and setTimeOut()

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import { scaleLinear, select } from "d3";
-
-const data = [
-  { x: 1.0, y: 1, r: 2 },
-  { x: 2.0, y: 2, r: 3 },
-  { x: 3.0, y: 3, r: 4 },
-  { x: 4.0, y: 4, r: 5 },
-  { x: 5.0, y: 5, r: 6 },
-];
+import { scaleLinear, select, scaleLog, scalePoint } from "d3";
 
 class Home extends Component {
-  state = {};
+  state = {
+    data: [
+      { x: 1, y: 1, r: 1 },
+      { x: 2, y: 2, r: 2 },
+      { x: 3, y: 3, r: 3 },
+      { x: 4, y: 4, r: 4 },
+      { x: 5, y: 5, r: 5 },
+    ],
+  };
 
   componentDidMount() {
-    this.display();
+    setTimeout(() => this.display(this.state.data, "red"), 1000);
+    setTimeout(() => this.display(this.state.data.slice(2), "green"), 2000);
+    setTimeout(() => this.display(this.state.data.slice(3), "blue"), 3000);
   }
 
-  display = () => {
-    console.log("display");
+  display = (data, color) => {
+    console.log(`Data : ${JSON.stringify(data)} Color : ${color}`);
     const xScale = scaleLinear()
       .domain([1, 5]) // Data space
       .range([100, 200]); // Pixel space
@@ -25,19 +27,23 @@ class Home extends Component {
     const yScale = scaleLinear()
       .domain([1, 5]) // Data space
       .range([1, 200]); // Pixel space
-    const rScale = scaleLinear()
+    const wScale = scaleLinear()
       .domain([1, 5]) // Data space
-      .range([1, 20]); // Pixel space
+      .range([10, 50]); // Pixel space
 
-    select("svg")
-      .selectAll("circle")
-      .data(data)
+    const rects = select("svg").selectAll("rect").data(data);
+
+    rects
       .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d.x))
-      .attr("cy", (d) => yScale(d.y))
-      .attr("r", (d) => rScale(d.r))
-      .attr("fill", "steelblue");
+      .append("rect")
+      .attr("x", (d) => xScale(d.x))
+      .attr("y", (d) => yScale(d.y))
+      .attr("width", (d) => wScale(d.r))
+      .attr("height", 50);
+
+    rects.attr("fill", color);
+
+    rects.exit().remove();
   };
 
   render() {
