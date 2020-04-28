2385c1dbdf178c27ee59f60f3b5978290c732216
Mohammad Kabir <mgkabir@gmail.com>
2020-04-28 23:34:21 +1000

    iris scatter points showing up

--- src/components/scatterPlot.jsx
+++ src/components/scatterPlot.jsx
@@ -8,14 +8,51 @@ class ScatterPlot extends Component {
   state = {};
 
   async componentDidMount() {
-    const response = await d3.json(irisEndPoint);
-    const extent = d3.extent(response, (d) => d.sepalLength);
-    //const maxSepalLength = d3.max(response, (d) => d.sepalLength);
+    // const data = [
+    //   { x: 1, y: 1, r: 1 },
+    //   { x: 2, y: 2, r: 2 },
+    //   { x: 3, y: 3, r: 3 },
+    //   { x: 4, y: 4, r: 4 },
+    //   { x: 5, y: 5, r: 5 },
+    // ];
 
-    console.log(`Extent ${extent}`);
+    const responseData = await d3.json(irisEndPoint);
+    const xScaleExtent = d3.extent(responseData, (d) => d.sepalLength);
+    const yScaleExtent = d3.extent(responseData, (d) => d.petalLength);
+
+    // const xScale = d3.scaleLinear().range([0, 250]);
+    // const yScale = d3.scaleLinear().range([250, 0]);
+
+    // xScale.domain(xScaleExtent);
+    // yScale.domain(yScaleExtent);
+
+    const xScale = d3.scaleLinear().domain([1, 10]).range([0, 600]);
+    const yScale = d3.scaleLinear().domain([1, 10]).range([400, 0]);
+
+    // Data bind
+    const circles = d3.select("svg").selectAll("circle").data(responseData);
+
+    // Enter
+    circles
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => xScale(d.sepalLength))
+      .attr("cy", (d) => yScale(d.petalLength))
+      .attr("r", 2);
+
+    // Update
+    circles.attr("cx", (d) => xScale(d.x)).attr("cy", (d) => yScale(d.y));
+
+    // Exit
+    circles.exit().remove();
   }
+
   render() {
-    return <h4>Scaller Plot with Iris Data</h4>;
+    return (
+      <React.Fragment>
+        <svg width="600" height="400"></svg>
+      </React.Fragment>
+    );
   }
 }
 
