9269e0975b07409e0bdef07fd47ec4400770d98f
Mohammad Kabir <mgkabir@gmail.com>
2020-04-28 23:53:55 +1000

    refactor scatter plot code

--- src/components/scatterPlot.jsx
+++ src/components/scatterPlot.jsx
@@ -8,41 +8,33 @@ class ScatterPlot extends Component {
   state = {};
 
   async componentDidMount() {
-    // const data = [
-    //   { x: 1, y: 1, r: 1 },
-    //   { x: 2, y: 2, r: 2 },
-    //   { x: 3, y: 3, r: 3 },
-    //   { x: 4, y: 4, r: 4 },
-    //   { x: 5, y: 5, r: 5 },
-    // ];
+    const circleRadius = 5;
+    const outerWidth = 600;
+    const outerHeight = 400;
+    const data = await d3.json(irisEndPoint); // fetch data
 
-    const responseData = await d3.json(irisEndPoint);
-    const xScaleExtent = d3.extent(responseData, (d) => d.sepalLength);
-    const yScaleExtent = d3.extent(responseData, (d) => d.petalLength);
+    const xScaleExtent = d3.extent(data, (d) => d.sepalLength);
+    const yScaleExtent = d3.extent(data, (d) => d.petalLength);
 
-    // const xScale = d3.scaleLinear().range([0, 250]);
-    // const yScale = d3.scaleLinear().range([250, 0]);
-
-    // xScale.domain(xScaleExtent);
-    // yScale.domain(yScaleExtent);
-
-    const xScale = d3.scaleLinear().domain([1, 10]).range([0, 600]);
-    const yScale = d3.scaleLinear().domain([1, 10]).range([400, 0]);
+    const xScale = d3.scaleLinear().domain(xScaleExtent).range([0, outerWidth]);
+    const yScale = d3
+      .scaleLinear()
+      .domain(yScaleExtent)
+      .range([outerHeight, 0]);
 
     // Data bind
-    const circles = d3.select("svg").selectAll("circle").data(responseData);
-
+    const circles = d3.select("svg").selectAll("circle").data(data);
     // Enter
     circles
       .enter()
       .append("circle")
       .attr("cx", (d) => xScale(d.sepalLength))
       .attr("cy", (d) => yScale(d.petalLength))
-      .attr("r", 2);
-
-    // Update
-    circles.attr("cx", (d) => xScale(d.x)).attr("cy", (d) => yScale(d.y));
-
+      .attr("r", circleRadius);
+    // Update : no-op ?
+    circles
+      .attr("cx", (d) => xScale(d.sepalLength))
+      .attr("cy", (d) => yScale(d.petalLength));
     // Exit
     circles.exit().remove();
   }
