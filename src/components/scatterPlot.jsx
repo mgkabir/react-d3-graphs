837ada78023221913aeeb0259a31dca8781bf424
Mohammad Kabir <mgkabir@gmail.com>
2020-04-30 13:42:16 +1000

    radius of circle is calculated based on sepalWidth

--- src/components/scatterPlot.jsx
+++ src/components/scatterPlot.jsx
@@ -9,7 +9,8 @@ class ScatterPlot extends Component {
 
   async componentDidMount() {
     const svg = d3.select("svg");
-    const circleRadius = 6;
+    const minRad = 2;
+    const maxRad = 10;
     const height = +svg.attr("height");
     const width = +svg.attr("width");
     const margin = { top: 20, right: 20, bottom: 20, left: 30 };
@@ -20,6 +21,7 @@ class ScatterPlot extends Component {
 
     const xScaleExtent = d3.extent(data, (d) => d.sepalLength);
     const yScaleExtent = d3.extent(data, (d) => d.petalLength);
+    const rScaleExtent = d3.extent(data, (d) => d.sepalWidth);
 
     const xScale = d3
       .scaleLinear()
@@ -31,6 +33,11 @@ class ScatterPlot extends Component {
       .domain(yScaleExtent)
       .range([height, 0])
       .nice();
+
+    const rScale = d3
+      .scaleLinear()
+      .domain(rScaleExtent)
+      .range([minRad, maxRad]);
     const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     const g = svg
@@ -44,7 +51,7 @@ class ScatterPlot extends Component {
       .append("circle")
       .attr("cx", (d) => xScale(d.sepalLength))
       .attr("cy", (d) => yScale(d.petalLength))
-      .attr("r", circleRadius)
+      .attr("r", (d) => rScale(d.sepalWidth))
       .attr("stroke", (d) => colorScale(d.species));
     // Update : no-op ?
     circles
